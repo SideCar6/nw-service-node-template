43039a33749ab7e9c7d4b4cb320b53be7b841d67
Merkes <matt.merkes@gmail.com>
2015-11-17 13:32:36 -0800

    Enable cors

--- server.js
+++ server.js
@@ -15,8 +15,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/test', function (req, res, next) {
   console.log(JSON.stringify(req.cookies, null, 2));
+  console.log(JSON.stringify(req.headers, null, 2));
   if (req.headers.cookie) {
     console.log(req.headers.cookie);
     request({
